3a97ace0dd8a444e3ed765359a283a3a06dce32b
Clinton Selke <clinuxrulz@gmail.com>
2025-08-16 14:24:33 +1000

    wip

--- src/music-editor/CodeGenCtx.ts
+++ src/music-editor/CodeGenCtx.ts
@@ -2,6 +2,7 @@ export class CodeGenCtx {
   private nextFieldIdx = 0;
   private fieldDeclarations = "";
   private constructorCode = "";
+  private messageHandlers = new Map<string,string>();
   private code_ = "";
 
   allocField(initValue: string): string {
@@ -14,17 +15,53 @@ export class CodeGenCtx {
     this.constructorCode += lines.map((line) => `      ${line}\r\n`).join("");
   }
 
+  insertMessageHandlerCode(type: string, lines: string[]) {
+    let handler = this.messageHandlers.get(type);
+    let toAdd = lines.map((line) => `      ${line}\r\n`).join("");
+    if (handler == undefined) {
+      handler = toAdd;
+    } else {
+      handler += toAdd;
+    }
+    this.messageHandlers.set(type, toAdd);
+  }
+
   insertCode(lines: string[]) {
     this.code_ += lines.map((line) => `      ${line}\r\n`).join("");
   }
 
   get code(): string {
+    let messageHandlerCode = "";
+    if (this.messageHandlers.size != 0) {
+      messageHandlerCode = [
+        "    this.port.onmessage = (e) => {\r\n",
+        "      let data = e.data;\r\n",
+        ...this.messageHandlers.entries().map(
+          ([ type, code, ], idx) => {
+            let start: string;
+            let isLast = idx == this.messageHandlers.size - 1;
+            if (idx == 0) {
+              start = "if";
+            } else {
+              start = "} else if";
+            }
+            return (
+              `${start} (data.type == ${type}) {\r\n` +
+              `${code}${isLast ? "}\r\n" : ""}`
+            );
+          },
+        ),
+        "    };\r\n",
+      ].join("");
+    } else {
+      messageHandlerCode = "";
+    }
     return [
       "class CompiledGraphAudioWorkletProcessor extends AudioWorkletProcessor {",
       this.fieldDeclarations,
       "  constructor(options) {",
       "    super();",
-      `${this.constructorCode}  }`,
+      `${this.constructorCode}${messageHandlerCode}  }`,
       "",
       "  process(inputs, outputs, parameters) {",
       "    let output = outputs[0][0];",
