970dc8bda28e507dc870351bda7a96528e4980a1
Clinton Selke <clinuxrulz@gmail.com>
2025-09-20 21:11:18 +1000

    wip

--- src/music-editor/Knob.tsx
+++ src/music-editor/Knob.tsx
@@ -9,7 +9,7 @@ const Knob: Component<{
   valueAt2Pi: number,
 }> = (props) => {
   let indentAngle = createMemo(() => {
-    return (props.value - props.valueAt0Pi) * 2.0 * Math.PI / (props.valueAt2Pi - props.valueAt0Pi);
+    return (props.value - props.valueAt0Pi) * 2.0 * Math.PI / (props.valueAt2Pi - props.valueAt0Pi) + 0.5;
   });
   let indentR = createMemo(() => 0.7 * 0.5 * props.size);
   let indentPos = createMemo(() => {
@@ -28,10 +28,23 @@ const Knob: Component<{
         "height": `${props.size}px`,
         "border-radius": "50%",
         //"background-image": "radial-gradient(circle at 30% 30%, #dddddd, #777777, #444444)",
-        "background-image": "conic-gradient(#888888, #ffffff, #888888, #ffffff, #888888, #ffffff, #888888)",
+        //"background-image": "conic-gradient(#888888, #ffffff, #888888, #ffffff, #888888, #ffffff, #888888)",
+        "background": "conic-gradient(#BBB, #444, #BBB)",
         "position": "relative",
       }}
     >
+      <div
+        style={{
+          "width": `${0.9*props.size}px`,
+          "height": `${0.9*props.size}px`,
+          "border-radius": "50%",
+          //"background-image": "radial-gradient(circle at 30% 30%, #dddddd, #777777, #444444)",
+          "background-image": "conic-gradient(#888888, #ffffff, #888888, #ffffff, #888888, #ffffff, #888888)",
+          "position": "absolute",
+          "left": `${0.05*props.size}px`,
+          "top": `${0.05*props.size}px`,
+        }}
+      />
       <div
         style={{
           "position": "absolute",
@@ -40,7 +53,8 @@ const Knob: Component<{
           "width": `${props.indentSize}px`,
           "height": `${props.indentSize}px`,
           "border-radius": "50%",
-          "background-image": "radial-gradient(circle at 30% 30%, #777777, #999999, #dddddd)",
+          //"background-image": "radial-gradient(circle at 30% 30%, #777777, #999999, #dddddd)",
+          "background-color": "#700"
         }}
       />
     </div>
