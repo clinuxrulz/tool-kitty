ff746357e77dfc2272f9233610f51d5fdb0310a0
Clinton Selke <clinuxrulz@gmail.com>
2025-10-19 22:41:14 +1000

    fixed a couple of bugs in TextureNode for model editor

--- packages/tool-kitty-model-editor/src/nodes/TextureNode.tsx
+++ packages/tool-kitty-model-editor/src/nodes/TextureNode.tsx
@@ -102,7 +102,7 @@ export class TextureNode implements Node<NodeTypeExt,NodeExt,TextureState> {
       let schema = compile.toSchema(code);
       onInit(({ gl, program, rerender, }) => {
         let uniforms = uniformView(gl, program, schema.uniforms);
-        uniforms[textureIdent].set(gl.TEXTURE0);
+        uniforms[textureIdent].set(0);
         let texture = gl.createTexture();
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
@@ -112,6 +112,12 @@ export class TextureNode implements Node<NodeTypeExt,NodeExt,TextureState> {
             if (image == undefined) {
               return;
             }
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+            // Set wrapping modes
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
             gl.texImage2D(
               gl.TEXTURE_2D,
               0,
